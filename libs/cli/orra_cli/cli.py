2af81721ea4b1f83ad7292e56d2f726684c5fc1b
Ezo Saleh <105397+ezodude@users.noreply.github.com>
2024-05-20 16:24:42 +0100

    CLI tidy up.

--- libs/cli/orra_cli/cli.py
+++ libs/cli/orra_cli/cli.py
@@ -1,33 +1,37 @@
+from typing import Annotated, Union
+
 import typer
 from rich import print
-from typing import Annotated, Union
 
 from . import __version__
 
 app = typer.Typer()
+
 
 def version_callback(value: bool):
     if value:
         print(f"CLI Version: {__version__}")
         raise typer.Exit()
 
+
 @app.callback(no_args_is_help=True)
 def callback(
-        version: Annotated[Union[bool, None],
-    typer.Option(
-        default="--version",
-        help="Show the version and exit.",
-        callback=version_callback,
-        is_eager=True
-    ),
-] = None) -> None:
+        version: Annotated[Union[bool, None], typer.Option(
+            default="--version",
+            help="Show the version and exit.",
+            callback=version_callback,
+            is_eager=True
+        ),
+        ] = None,) -> None:
     """
     Orra - orra is great!
     """
 
+
 @app.command()
 def run():
     print("Running Orra app.")
+
 
 def main():
     app()
