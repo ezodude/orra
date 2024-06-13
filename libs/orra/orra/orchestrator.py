bcd099153c2e1e1aef72793a9b04f031d35e8978
Ezo Saleh <105397+ezodude@users.noreply.github.com>
2024-06-13 22:37:37 +0100

    Updated code to match new vision.

--- libs/orra/orra/orchestrator.py
+++ libs/orra/orra/orchestrator.py
@@ -24,14 +24,14 @@ class Orra:
         self._steps = []
         self._StateDict = create_typed_dict("StateDict", schema)
         self._StepResponseModel = create_response_model(self._StateDict)
-        self._workflow = StateGraph(self._StateDict)
-        self._compiled_workflow = None
+        self._flow = StateGraph(self._StateDict)
+        self._compiled_flow = None
 
     def step(self, func: Callable) -> Callable:
         self._register(func)
         response_model = self._StepResponseModel
 
-        @self._steps_app.post(f"/workflow/{func.__name__}")
+        @self._steps_app.post(f"/flow/{func.__name__}")
         async def wrap_endpoint(v: response_model):
             if inspect.iscoroutinefunction(func):
                 return await func(v.dict())
@@ -44,19 +44,19 @@ class Orra:
         if debug:
             printer.print("Initialising \[debug] mode...Done!")
 
-        msg = "Compiling Orra application workflow..."
-        self._compiled_workflow = self._compile(self._workflow, self._steps)
+        msg = "Compiling Orra application flow..."
+        self._compiled_flow = self._compile(self._flow, self._steps)
         msg = f"{msg} Done!"
         printer.print(msg)
 
         printer.print("Prepared Orra application step endpoints...Done!")
 
-        msg = "Preparing Orra application workflow endpoint..."
+        msg = "Preparing Orra application flow endpoint..."
         response_model = self._StepResponseModel
 
-        @self._steps_app.post(f"/workflow")
-        async def wrap_workflow(v: response_model):
-            return self._compiled_workflow.invoke(v.dict(), debug=debug)
+        @self._steps_app.post(f"/flow")
+        async def wrap_flow(v: response_model):
+            return self._compiled_flow.invoke(v.dict(), debug=debug)
 
         msg = f"{msg} Done!"
         printer.print(msg)
@@ -66,8 +66,8 @@ class Orra:
         return self._steps_app
 
     def execute(self) -> None:
-        self._compiled_workflow = self._compile(self._workflow, self._steps)
-        self._compiled_workflow.invoke({})
+        self._compiled_flow = self._compile(self._flow, self._steps)
+        self._compiled_flow.invoke({})
 
     def _register(self, func: Callable):
         if inspect.iscoroutinefunction(func):
@@ -77,19 +77,19 @@ class Orra:
         else:
             executable_func = func
 
-        self._workflow.add_node(func.__name__, executable_func)
+        self._flow.add_node(func.__name__, executable_func)
         self._steps.append(func.__name__)
 
     @staticmethod
-    def _compile(workflow, steps):
+    def _compile(flow, steps):
         parts = ""
         for i in range(len(steps) - 1):
             parts = f"{steps[i]} -> {steps[i + 1]}" if parts == "" else f"{parts} -> {steps[i + 1]}"
-            workflow.add_edge(steps[i], steps[i + 1])
+            flow.add_edge(steps[i], steps[i + 1])
 
         if len(steps) > 1:
-            workflow.set_entry_point(steps[0])
-            workflow.add_edge(steps[-1], END)
+            flow.set_entry_point(steps[0])
+            flow.add_edge(steps[-1], END)
 
-        # print("compiling workflow steps:", parts)
-        return workflow.compile()
+        # print("compiling flow steps:", parts)
+        return flow.compile()
